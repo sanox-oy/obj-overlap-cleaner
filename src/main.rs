c307d1c466f0fcb9fbe7c03da7cec79dfaddb18f
santeriok <santeri.oksanen@gmail.com>
2025-09-05 22:06:35 +0300

    Create target folder if it doesn't exist

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use std::{ffi::OsString, time::Instant};
+use std::{ffi::OsString, path::PathBuf, time::Instant};
 
 mod grid;
 mod io;
@@ -27,6 +27,11 @@ fn main() {
     let start_time = Instant::now();
 
     println!("Running with args: {args:?}");
+
+    // Create out-folder if it doesn't exist
+    let out_path = PathBuf::from(&args.out_folder);
+    std::fs::create_dir_all(out_path)
+        .unwrap_or_else(|_| panic!("Couldn't create output directory: {:?}", args.out_folder));
 
     let mut assets =
         world::WorldAssets::new(args.normal_asset_folder, args.hq_asset_folders.clone());
